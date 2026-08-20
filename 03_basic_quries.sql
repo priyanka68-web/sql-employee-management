84282aa98d232e4e8d15007a37a3a80f853f427a
priyanka68-web <priyanka.anireddy@gmail.com>
2026-08-20 09:37:40 -0400

    Update 03_basic_quries.sql
    Using average

--- 03_basic_quries.sql
+++ 03_basic_quries.sql
@@ -11,3 +11,11 @@ SELECT e.name,d.dept_name
 FROM employeese
 JOIN departments d
   ON e.dept_id=d.dept_id;
+
+--Question 3
+--Find the average salary for each department
+select d.dept_name,AVG(e.salary) AS avg_salary 
+from employees e
+join departments d
+on e.dept_id=d.dept_id
+group by d.dept_name;
