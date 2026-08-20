dfb23f4bfa663c74b3e158c9752b7dbf608948a5
priyanka68-web <priyanka.anireddy@gmail.com>
2026-08-20 09:30:36 -0400

    Update 03_basic_quries.sql
    Add basic SQL queries

--- 03_basic_quries.sql
+++ 03_basic_quries.sql
@@ -4,3 +4,10 @@ Find employess earning more than $80,000
 SELECT name,salary
 FROM employees
 WHERE salary > 80000;
+
+--Question 2
+--Find the employee name and department name for all employees
+SELECT e.name,d.dept_name
+FROM employeese
+JOIN departments d
+  ON e.dept_id=d.dept_id;
